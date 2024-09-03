1f6662641152b3eb1311a5e77c57ccaf2e87af0f
Daniel-Brd <dev.danielbrandao@gmail.com>
2024-09-03 19:58:14 -0300

    feat: add class validator on create task DTO

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,12 @@
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(128)
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   description: string;
 }
